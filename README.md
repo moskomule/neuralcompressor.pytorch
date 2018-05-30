21ad117712c1e51598c60792b435f477f8fe28ae
moskomule <hataya@keio.jp>
2018-05-30 17:11:27 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# neuralcompressor.pytorch
+
+An implementation of neuralcompressor, proposed in [*Compressing Word Embeddings via Deep Compositional Code Learning*](http://arxiv.org/abs/1711.01068) by Raphael Shu and Hideki Nakayama (ICLR, 2018).
+
+The status of this repository is *WORK IN PROGRESS*.
+
+# Prerequirements
+
+* `Python >= 3.6`
+* `PyTorch >= 4.0`
+* (`torchtext`)
+
+# Results
+
+# Plan
+
+* [ ] Compress Glove
+* [ ] Add IMDB sentimental analysis example
---
+++ models.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.distributions.relaxed_categorical import RelaxedOneHotCategorical as GumbelSoftmax
+
+
+class Encoder(nn.Module):
+    def __init__(self, dim_emb, num_component, num_codevec):
+        super(Encoder, self).__init__()
+        self.hidden = nn.Linear(dim_emb, num_component * num_codevec // 2)
+        self.out = nn.ModuleList([nn.Linear(num_component * num_codevec // 2, num_codevec)
+                                  for _ in range(num_component)])
+        self.register_buffer("_temperature", torch.tensor(1.0))
+
+    def forward(self, input):
+        x = F.tanh(self.hidden(input))
+        x = [F.softplus(m(x)) for m in self.out]
+        return [GumbelSoftmax(self._temperature, p).rsample() for p in x]
+
+    def get_code(self, input):
+        with torch.no_grad():
+            probs = torch.stack(self(input))
+            # num_component x batch x num_codevec
+            return probs.transpose(0, 1).argmax(dim=2)
+
+    @property
+    def temperature(self):
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, x):
+        self._temperature.fill_(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, dim_emb, num_component, num_codevec):
+        super(Decoder, self).__init__()
+        self.emb = nn.ModuleList([nn.Linear(num_codevec, dim_emb)
+                                  for _ in range(num_component)])
+
+    def forward(self, input):
+        return sum([m(v) for m, v in zip(self.emb, input)])
+
+
+if __name__ == "__main__":
+    dim_emb = 200
+    num_component = 4
+    num_codevec = 4
+    a = torch.rand(3, dim_emb)
+    enc = Encoder(dim_emb, num_component, num_codevec)
+    dec = Decoder(dim_emb, num_component, num_codevec)
+    loss = F.mse_loss(dec(enc(a)), a)
+    loss.backward()
+    print(enc.get_code(a))
